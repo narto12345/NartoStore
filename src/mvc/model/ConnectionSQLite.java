c64bc80b0b5341e4ba69d8ab75b5dd250c70074a
Nicolas <nicosan12@hotmail.com>
2023-09-17 12:05:33 -0500

    Se añade documentación a la clase ConnectionSQLite

--- src/mvc/model/ConnectionSQLite.java
+++ src/mvc/model/ConnectionSQLite.java
@@ -6,14 +6,25 @@ import java.sql.ResultSet;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+/**
+ * This class provides the object that allows you to connect to the SQLite
+ * database
+ */
 public class ConnectionSQLite {
 
     private static final String URL = "jdbc:sqlite:narto_store.db";
 
+    /**
+     * Returns the connection to the SQLite database
+     */
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL);
     }
 
+    /**
+     * Closes a ResultSet object
+     * @param rs ResultSet object to close
+     */
     public static void close(ResultSet rs) {
         try {
             rs.close();
@@ -22,6 +33,10 @@ public class ConnectionSQLite {
         }
     }
 
+    /**
+     * Closes a PreparedStatement object
+     * @param ps PreparedStatement object to close
+     */
     public static void close(PreparedStatement ps) {
         try {
             ps.close();
@@ -30,6 +45,10 @@ public class ConnectionSQLite {
         }
     }
 
+    /**
+     * Close a connection to a database
+     * @param conn Reference to the Connection object that you want to close
+     */
     public static void close(Connection conn) {
         try {
             conn.close();
